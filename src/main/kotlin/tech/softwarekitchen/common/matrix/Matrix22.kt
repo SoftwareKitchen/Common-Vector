a89f117e533e84870579bc6307d235836ee23b87
Tahlaria <tahlaria@software-kitchen.tech>
2023-05-18 22:37:48 +0200

    ADDED - basic 2d matrix stuff

---
+++ src/main/kotlin/tech/softwarekitchen/common/matrix/Matrix22.kt
@@ -0,0 +1,29 @@
+package tech.softwarekitchen.common.matrix
+
+import tech.softwarekitchen.common.vector.Vector2
+import kotlin.math.cos
+import kotlin.math.sin
+
+/**
+ * Usage:
+ * / xx yx \
+ * \ xy yy /
+ */
+class Matrix22(val xx: Double, val yx: Double, val xy: Double, val yy: Double){
+    companion object{
+        fun getRotationMatrix(rad: Double): Matrix22{
+            return Matrix22(cos(rad), sin(rad), -sin(rad), cos(rad))
+        }
+        fun getScaleMatrix(xFac: Double, yFac: Double): Matrix22{
+            return Matrix22(xFac,0.0,0.0,yFac)
+        }
+    }
+    fun mul(vec: Vector2): Vector2 {
+        return Vector2(vec.x * xx + vec.y * yx, vec.x * xy + vec.y * yy)
+    }
+
+    fun invert(): Matrix22{
+        return Matrix22(yy,-yx,-xy,xx)
+    }
+
+}
--- src/main/kotlin/tech/softwarekitchen/common/vector/Vector2.kt
+++ src/main/kotlin/tech/softwarekitchen/common/vector/Vector2.kt
@@ -4,4 +4,12 @@ data class Vector2(val x: Double, val y: Double){
     fun plus(other: Vector2): Vector2 {
         return Vector2(x+other.x, y+other.y)
     }
+
+    fun minus(other: Vector2): Vector2{
+        return Vector2(x - other.x, y - other.y)
+    }
+
+    fun scale(fac: Double): Vector2{
+        return Vector2(x * fac, y * fac)
+    }
 }
