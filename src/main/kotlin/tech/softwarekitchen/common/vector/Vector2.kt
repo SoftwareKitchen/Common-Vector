25891f31b732f8d9b7e960d237d7cd2b59190314
Tahlaria <tahlaria@software-kitchen.tech>
2023-07-22 21:37:59 +0200

    CHANGED - Version to 1.4, ADDED - some v2 functions

--- src/main/kotlin/tech/softwarekitchen/common/vector/Vector2.kt
+++ src/main/kotlin/tech/softwarekitchen/common/vector/Vector2.kt
@@ -1,7 +1,6 @@
 package tech.softwarekitchen.common.vector
 
 import kotlin.math.hypot
-import kotlin.math.sqrt
 
 data class Vector2(val x: Double, val y: Double){
     fun plus(other: Vector2): Vector2 {
@@ -35,5 +34,21 @@ data class Vector2(val x: Double, val y: Double){
     fun norm(): Vector2{
         val l = length()
         return Vector2(x / l, y / l)
+    }
+
+    fun dist(other: Vector2): Double{
+        return minus(other).length();
+    }
+
+    fun toVector2i(): Vector2i{
+        return Vector2i(x.toInt(), y.toInt())
+    }
+
+    fun invertY(): Vector2{
+        return Vector2(x,-y)
+    }
+
+    fun scalar(other: Vector2): Double{
+        return x * other.x + y * other.y
     }
 }
