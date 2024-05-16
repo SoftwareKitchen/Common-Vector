c1efd3adb28da833f737cbf690ef9546dc636701
Tahlaria <tahlaria@software-kitchen.tech>
2024-05-16 19:06:09 +0200

    CHANGED - Version to 1.5, changed some vector2i methods to operators

--- src/main/kotlin/tech/softwarekitchen/common/vector/Vector2i.kt
+++ src/main/kotlin/tech/softwarekitchen/common/vector/Vector2i.kt
@@ -3,13 +3,13 @@ package tech.softwarekitchen.common.vector
 import kotlin.math.roundToInt
 
 class Vector2i(val x: Int, val y: Int){
-    fun plus(other: Vector2i): Vector2i{
+    operator fun plus(other: Vector2i): Vector2i{
         return Vector2i(x+other.x, y+other.y)
     }
     fun invert(): Vector2i{
         return Vector2i(-x, -y)
     }
-    fun scale(fac: Double): Vector2i{
+    operator fun times(fac: Double): Vector2i{
         return Vector2i((x * fac).roundToInt(), (y * fac).roundToInt())
     }
 
