295f676b37b19ee873165914f8951838800dbda1
Tahlaria <tahlaria@software-kitchen.tech>
2024-05-20 08:46:38 +0200

    Added some more operators for Vector2i

--- src/main/kotlin/tech/softwarekitchen/common/vector/Vector2i.kt
+++ src/main/kotlin/tech/softwarekitchen/common/vector/Vector2i.kt
@@ -1,16 +1,40 @@
 package tech.softwarekitchen.common.vector
 
+import java.math.BigDecimal
+import java.math.BigInteger
 import kotlin.math.roundToInt
 
 class Vector2i(val x: Int, val y: Int){
     operator fun plus(other: Vector2i): Vector2i{
         return Vector2i(x+other.x, y+other.y)
     }
+
+    operator fun minus(other: Vector2i): Vector2i{
+        return Vector2i(x - other.x, y - other.y)
+    }
     fun invert(): Vector2i{
         return Vector2i(-x, -y)
     }
     operator fun times(fac: Double): Vector2i{
         return Vector2i((x * fac).roundToInt(), (y * fac).roundToInt())
+    }
+
+    operator fun times(fac: Int): Vector2i{
+        return Vector2i(x * fac, y * fac)
+    }
+
+    operator fun times(fac: BigDecimal): Vector2i{
+        val asDouble = fac.toDouble()
+        return this * asDouble
+    }
+
+    operator fun times(fac: BigInteger): Vector2i{
+        val asInt = fac.toInt()
+        return this * asInt
+    }
+
+    fun scale(fac: Double): Vector2i{
+        return this * fac
     }
 
     override fun equals(other: Any?): Boolean {
